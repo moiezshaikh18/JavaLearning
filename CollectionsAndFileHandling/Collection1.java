aa01a205eb21c8327df7c1870683527d972e27c3
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-08-20 20:54:20 +0530

    Collection printing arrayList

---
+++ CollectionsAndFileHandling/Collection1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Collection1 {
+    public static void main(String[] args) {
+        ArrayList l1 = new ArrayList<>(5);
+
+        l1.add("A");
+        l1.add("b");
+        l1.add("c");
+        l1.add("d");
+        l1.add("e");
+
+        System.out.println(l1);
+
+    }
+}
